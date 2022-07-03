c5ef3c838f7de5e241e98a19f402688b9ffc77ef
Diego Dias <diego2007jf@gmail.com>
2022-07-03 01:30:20 -0300

    working string matching on character selection

--- src/app/character-selection/character-selection.component.ts
+++ src/app/character-selection/character-selection.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Observable, Subject, map, Subscriber } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { BackendService } from '../services/backend.service';
 
 
@@ -13,6 +13,8 @@ export class CharacterSelectionComponent implements OnInit {
 
   characterObservables: Observable<string[]>[] = [];
   optionSelected: Subject<string> = new Subject();
+
+  matchedOptions: string[] = [];
   formGroup: FormGroup = this._fb.group({
     'characterName' : ['']
   })
@@ -20,7 +22,30 @@ export class CharacterSelectionComponent implements OnInit {
   constructor(
     private _backend: BackendService, 
     private _fb: FormBuilder
-  ) { }
+  ) { this.optionSelected.subscribe((option: string) => {
+        this.matchedOptions = [];
+
+        this.characterObservables[0].subscribe((names: string[]) => {
+          this._getMatchedNames(names, option).
+            forEach((name: string) => this.matchedOptions.push(`(BBCF) ${name}`));
+        });
+
+        this.characterObservables[1].subscribe((names: string[]) => {
+          this._getMatchedNames(names, option).
+            forEach((name: string) => this.matchedOptions.push(`(BBTAG) ${name}`));
+        });
+  })}
+
+  private _parseCharacterName(characterName: string): string {
+    return characterName.replace('_', ' ');
+  }
+
+  private _getMatchedNames(characterNames: string[], option: string): string[] {
+    let matchedOptions: string[] = characterNames.filter(
+      (name: string) => name.toLowerCase().includes(option.toLowerCase())
+      );
+    return matchedOptions.map(this._parseCharacterName);
+  }
 
   initForm() {
     const formObservable = this.formGroup.get('characterName')?.valueChanges;
