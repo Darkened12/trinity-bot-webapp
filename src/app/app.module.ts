0463df2b72be2751d55745c0d4995568656122b3
Diego Dias <diego2007jf@gmail.com>
2022-07-07 05:19:17 -0300

    added character component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CharacterComponent } from './character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrinityHeaderComponent,
-    CharacterSelectionComponent
+    CharacterSelectionComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
