e7a61d20a1dcf5082ae3c0465e1e28c8b76a6806
Diego Dias <diego2007jf@gmail.com>
2022-07-09 02:23:44 -0300

    functioning move view

--- src/app/move-info/move-info.component.ts
+++ src/app/move-info/move-info.component.ts
@@ -11,10 +11,43 @@ import { UrlRouterParsingService } from '../services/url-router-parsing.service'
 })
 export class MoveInfoComponent implements OnInit {
   moves: Subject<Array<IMove>> = new Subject();
+  moveProperties: string[] = [
+    'damage',
+    'startup',
+    'active',
+    'recovery',
+    'frame_adv',
+    'guard',
+    'attribute',
+    'invul',
+    'blockstun',
+    'blockstop',
+    'P1',
+    'P2',
+    'cancel'
+  ];
   constructor(private _backend: BackendService, private urlParser: UrlRouterParsingService) { }
 
   getSpriteUrl(url: string): string {
     return `${this._backend.endpoint}${this.urlParser.gamePrefix}/${url}`;
+  }
+
+  private toTitle(str: string): string {
+    let sentence = str.toLowerCase().split(" ");
+    for (let i = 0; i < sentence.length; i++) {
+      sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+    
+    return sentence.join(" ");
+  }
+
+  parseProperty(property: string): string {
+    return this.toTitle(property.split('_').join(' '));
+  }
+
+  getMoveValueByProperty(move: IMove, property: string): string {
+    const objKey = property as keyof typeof move;
+    return move[objKey];
   }
 
   ngOnInit(): void {
