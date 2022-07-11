a6714efd69c3b129139f92111723758325a4c459
Diego Dias <diego2007jf@gmail.com>
2022-07-11 08:06:19 -0300

    implemented trinity-header on character-view

--- src/app/trinity-header/trinity-header.component.ts
+++ src/app/trinity-header/trinity-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trinity-header',
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trinity-header.component.css']
 })
 export class TrinityHeaderComponent implements OnInit {
-
+  @Input() isSmall: boolean = false; 
   constructor() { }
+
+  getImgStyle(): string {
+    if (this.isSmall) { 
+      return 'width: 15%; height: 15%; margin-left: 30px';
+    }
+    return ';';
+  }
 
   ngOnInit(): void {
   }
