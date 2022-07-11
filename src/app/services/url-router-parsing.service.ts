a5cf8b48420cde503682c1314715ae4738066cbc
Diego Dias <diego2007jf@gmail.com>
2022-07-11 04:40:23 -0300

    improved logic

--- src/app/services/url-router-parsing.service.ts
+++ src/app/services/url-router-parsing.service.ts
@@ -1,45 +1,56 @@
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router} from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, filter, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlRouterParsingService {
-  characterName: BehaviorSubject<string> = new BehaviorSubject('');
-  gamePrefix: BehaviorSubject<string> = new BehaviorSubject('');
-  moveAnchor: BehaviorSubject<string> = new BehaviorSubject('');
-
-  parseEmptyValue(value: string): boolean {
-    return value !== '';
-  }
+  private _characterNameEmmiter: BehaviorSubject<string> = new BehaviorSubject('');
+  private _gamePrefixEmmiter: BehaviorSubject<string> = new BehaviorSubject('');
+  private _moveAnchorEmmiter: BehaviorSubject<string> = new BehaviorSubject('');
 
   constructor(private _router: Router) { 
     this._router.events.subscribe((events) => {
       if (events instanceof NavigationEnd) {
-        const url: string = events.url;
-        const segments: string[] = url.split('/').slice(1);
-        if (segments[0] !== undefined && segments[0] !== this.gamePrefix.getValue()) {
-          this.gamePrefix.next(segments[0]);
+        const segments: string[] = this._splitUrlInSegments(events.url);
+        if (segments[0] !== undefined && segments[0] !== this._gamePrefixEmmiter.getValue()) {
+          this._gamePrefixEmmiter.next(segments[0]);
         }
         if (segments[1] !== undefined) {
           const characterPath: string = segments[1]
           if (characterPath.includes('#')) {
             const tempSegments: string[] = characterPath.split('#');
-            if (this.characterName.getValue() !== tempSegments[0]) { 
-              this.characterName.next(tempSegments[0]);
-              this.moveAnchor.next(tempSegments[1]) 
+            if (this._characterNameEmmiter.getValue() !== tempSegments[0]) { 
+              this._characterNameEmmiter.next(tempSegments[0]);
+              this._moveAnchorEmmiter.next(tempSegments[1]) 
             }
             
           }
-          else { this.characterName.next(characterPath); }
+          else { this._characterNameEmmiter.next(characterPath); }
         }
       }
 
     })
   }
 
-  hasEmmited(): boolean {
-    return this.gamePrefix.getValue() !== '' || this.characterName.getValue() !== '' || this.moveAnchor.getValue() !== '';
+  private _splitUrlInSegments(url: string): string[] {
+    return url.split('/').slice(1);
+  }
+
+  private _parseEmptyValue(value: string): boolean {
+    return value !== '';
+  }
+
+  gamePrefix(): Observable<string> {
+    return this._gamePrefixEmmiter.pipe(filter(this._parseEmptyValue));
+  }
+
+  characterName(): Observable<string> {
+    return this._characterNameEmmiter.pipe(filter(this._parseEmptyValue));
+  }
+
+  moveAnchor(): Observable<string> {
+    return this._moveAnchorEmmiter.pipe(filter(this._parseEmptyValue));
   }
 }
