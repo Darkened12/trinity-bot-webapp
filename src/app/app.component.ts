224a78a1b664fe2eb77c6fd3ca2a7cfc4ca258b8
Diego Dias <diego2007jf@gmail.com>
2022-07-28 14:17:01 -0300

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { BackendService } from './services/backend.service';
-
 
 @Component({
   selector: 'app-root',
@@ -9,15 +7,9 @@ import { BackendService } from './services/backend.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title: string = 'trinity-frame-data';
-  cf_character_names: string[] = [];
-  tag_character_names: string[] = [];
 
-  constructor(private backend: BackendService) {}
+  constructor() {}
   
-  ngOnInit() {
-    // const character_names: string[][] = this.backend.getAllCharacterNames();
-    // this.cf_character_names = character_names[0];
-    // this.tag_character_names = character_names[1];
-  }
+  ngOnInit() {}
+  
 }
