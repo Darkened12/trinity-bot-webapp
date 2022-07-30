ab60e58d36b9498962265a4a8de00e23c606b897
Diego Dias <diego2007jf@gmail.com>
2022-07-30 06:34:18 -0300

    updated

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInDownBigOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  styleUrls: ['./page-not-found.component.css'],
+  animations: [
+    fadeInDownBigOnEnterAnimation()
+  ]
 })
 export class PageNotFoundComponent implements OnInit {
 
