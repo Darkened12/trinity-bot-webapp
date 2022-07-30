61100df35720dd0049c76f414e12ca494b6f443c
Diego Dias <diego2007jf@gmail.com>
2022-07-30 00:31:56 -0300

    added angular-animations module

--- src/app/character-selection-view/character-selection-view.component.ts
+++ src/app/character-selection-view/character-selection-view.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInDownOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-character-selection-view',
   templateUrl: './character-selection-view.component.html',
-  styleUrls: ['./character-selection-view.component.css']
+  styleUrls: ['./character-selection-view.component.css'],
+  animations: [
+    fadeInDownOnEnterAnimation()
+  ]
 })
 export class CharacterSelectionViewComponent implements OnInit {
   textInputClass = 'form-control-lg';
