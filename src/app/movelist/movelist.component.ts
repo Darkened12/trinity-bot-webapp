65af41a895cb04b6e1a0bb8ae709e9ed6bfed591
Diego Dias <diego2007jf@gmail.com>
2022-12-04 11:10:26 -0300

    Implemented move type ordering

--- src/app/movelist/movelist.component.ts
+++ src/app/movelist/movelist.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { fadeInLeftOnEnterAnimation } from 'angular-animations';
-import { Observable } from 'rxjs';
+import { map, Observable, ReplaySubject } from 'rxjs';
 import { IMoveList } from '../services/backend.models';
 
 @Component({
@@ -13,9 +13,25 @@ import { IMoveList } from '../services/backend.models';
 })
 export class MovelistComponent implements OnInit {
   @Input() moveList!: Observable<IMoveList[]>;
+  orderedMoveList: ReplaySubject<IMoveList[]> = new ReplaySubject(1);
 
   constructor() { }
 
-  ngOnInit(): void { }
+  swapPositions(array: Array<any>, a: number, b: number): void {
+    [array[a], array[b]] = [array[b], array[a]]
+  }
+
+  ngOnInit(): void {
+    this.moveList.pipe(
+      map((moveList: IMoveList[]) => {
+        this.swapPositions(moveList, 2, 0)
+        this.swapPositions(moveList, 1, 2)
+        return moveList
+      })
+    ).subscribe((moveList: IMoveList[]) => {
+      this.orderedMoveList.next(moveList);
+    }) 
+  
+  }
 
 }
