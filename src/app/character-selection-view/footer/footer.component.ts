0e62c19d0f71d0046632a34d713037e3728b1ad7
Diego Dias <diego2007jf@gmail.com>
2023-07-19 20:30:05 -0300

    updated endpoint

--- src/app/character-selection-view/footer/footer.component.ts
+++ src/app/character-selection-view/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected backend: BackendService) { }
 
   ngOnInit(): void {
   }
